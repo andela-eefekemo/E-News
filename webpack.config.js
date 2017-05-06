86e76c3cff3117ba2d7ea08e15d407621d31e5d7
andela-eefekemo <eguonoghene.efekemo@andela.com>
2017-05-06 14:02:00 +0100

    setup webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+export default {
+  entry: './src/js/main.js',
+  output: {
+    filename: '.js/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+        },
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  watch: true,
+};
