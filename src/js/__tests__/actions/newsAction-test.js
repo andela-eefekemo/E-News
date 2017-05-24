893e9346304036013e3f5b6c3ed241a0abdd72e9
andela-eefekemo <eguonoghene.efekemo@andela.com>
2017-05-24 12:44:54 +0100

    Add more actions test

--- src/js/__tests__/actions/newsAction-test.js
+++ src/js/__tests__/actions/newsAction-test.js
@@ -1,5 +1,5 @@
 /* global expect jest test */
-// import dispatcher from '../../dispatcher';
+import dispatcher from '../../dispatcher';
 import * as newsActions from '../../actions/newsAction';
 import newsApi from '../../utils/newsApi';
 
@@ -13,6 +13,7 @@ jest.mock('../../utils/newsApi');
 const newsApiGet = newsApi.get;
 
 newsApiGet.mockReturnValue(Promise.resolve({ data: 'This is the data' }));
+const dispatchSpy = jest.spyOn(dispatcher, 'dispatch');
 
 test('should call newsActions.getSources() on getSources', () => {
   newsActions.getSources();
@@ -22,4 +23,18 @@ test('should call newsActions.getSources() on getSources', () => {
 test('should call newsActions.getArticles()', () => {
   newsActions.getArticles();
   expect(newsApiGet.mock.calls.length).toBe(2);
+});
+
+test('', () => {
+  newsActions.getSources();
+  const arg = dispatchSpy.mock.calls[0][0];
+  expect(dispatchSpy).toHaveBeenCalled();
+  expect(arg.type).toEqual('GET_SOURCES');
+});
+
+test('', () => {
+  newsActions.getArticles();
+  const arg = dispatchSpy.mock.calls[0][0];
+  expect(dispatchSpy).toHaveBeenCalled();
+  expect(arg.type).toEqual('GET_SOURCES');
 });
